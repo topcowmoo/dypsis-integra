1a51c370aed742e87c8ac161daf28a9649fff2e8
Salvatore <mammoliti.sam@gmail.com>
2024-09-28 12:50:25 -0400

    changed port in vite config

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,15 @@
 function Footer() {
-    return (
-        <footer>
-        <p>Footer</p>
-        </footer>
-    );
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer>
+      <p>Salvatore Mammoliti</p>
+
+      <div className="text-[14px]">
+        <span>&copy; {currentYear}</span>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
