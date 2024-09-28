2d82622ca4ed143bf1f13eb7efc8b22d730c6101
Salvatore <mammoliti.sam@gmail.com>
2024-09-28 12:38:42 -0400

    moved src and root out of vite project folder

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App.jsx';
 import Home from './pages/Home.jsx';
 import Error from './pages/Error.jsx';
+import './index.css';
 
 const router = createBrowserRouter([
   {
