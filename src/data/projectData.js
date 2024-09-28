088632f4235b9fbf6849025ed24e7c6f48587b3b
Salvatore <mammoliti.sam@gmail.com>
2024-09-28 13:57:50 -0400

    created projectdata file

---
+++ src/data/projectData.js
@@ -0,0 +1,60 @@
+const projectData = [
+    {
+      id: 1,
+      title: "Dry MVC Tech Blog",
+      description:
+        "A CMS-style blog platform with user-generated content, built on the MVC architecture.",
+      image: "https://portfoliobucket2024.s3.amazonaws.com/drymvc.webp",
+      githubLink: "https://github.com/topcowmoo/dry-mvc-tech-blog",
+      herokuLink: "https://dry-mvc-tech-blog-b103946e224c.herokuapp.com/",
+    },
+    {
+      id: 2,
+      title: "Baketomo",
+      description:
+        "A multi-page MERN app featuring GraphQL, AWS S3, and MongoDB Atlas, deployed on Render.",
+      image: "https://portfoliobucket2024.s3.amazonaws.com/baketomo.webp",
+      githubLink: "https://github.com/topcowmoo/futuristic-wound",
+      renderLink: "https://baketomo.onrender.com",
+    },
+    {
+      id: 3,
+      title: "Just Another Text Editor",
+      description:
+        "A Progressive Web Application (PWA) for rich text editing with offline support.",
+      image: "https://portfoliobucket2024.s3.amazonaws.com/jate.webp",
+      githubLink: "https://github.com/topcowmoo/general-toothpaste",
+      renderLink: "https://general-toothpaste.onrender.com",
+    },
+    {
+      id: 4,
+      title: "Unbreakable Cream",
+      description:
+        "A recipe-sharing platform with robust community interaction for home cooking enthusiasts.",
+      image: "https://portfoliobucket2024.s3.amazonaws.com/cream.webp",
+      githubLink: "https://github.com/topcowmoo/unbreakable-cream",
+      herokuLink: "https://unbreakable-cream-a4b95d823c2c.herokuapp.com/",
+    },
+    {
+      id: 5,
+      title: "Windy E-Commerce",
+      description:
+        "An Express.js and MySQL-powered e-commerce platform with scalable retail features.",
+      image: "https://portfoliobucket2024.s3.amazonaws.com/windy.webp",
+      githubLink: "https://github.com/topcowmoo/windy-e-commerce-site",
+      demoLink:
+        "https://drive.google.com/file/d/1q_LhyzKBZ6-6fw9hbPgev_rvDWNz-TUh/view",
+    },
+    {
+      id: 6,
+      title: "Weather Interface",
+      description:
+        "A responsive weather dashboard delivering real-time updates and forecasts.",
+      image: "https://portfoliobucket2024.s3.amazonaws.com/weather.webp",
+      githubLink: "https://github.com/topcowmoo/effective-weather-interface",
+      githubPagesLink: "https://topcowmoo.github.io/effective-weather-interface/",
+    },
+  ];
+  
+  export default projectData;
+  
