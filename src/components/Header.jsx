b7f51ec56d3f8915434c3b3abe49c1af53e0a4b5
Salvatore <mammoliti.sam@gmail.com>
2024-09-29 09:41:59 -0400

    added test logo to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 import { HashLink } from "react-router-hash-link";
 import { RxHamburgerMenu, RxCross1 } from "react-icons/rx";
 import { FiSun, FiMoon } from "react-icons/fi";
+import logoDark from '../assets/logodark.png';
+import logoLight from '../assets/logolight.png';
 
 function Header({ toggleDarkMode, isDarkMode }) {
   const [isActive, setIsActive] = useState(false); // Mobile menu state
@@ -13,10 +15,63 @@ function Header({ toggleDarkMode, isDarkMode }) {
   };
 
   return (
-    <header className="relative flex justify-center items-center top-0 z-50 w-full">
-      <div className="flex justify-center items-center p-10 text-[20px] w-full max-w-screen-xl mx-auto">
+    <header className="relative flex justify-between items-center top-0 z-50 w-full p-5 bg-transparent">
+      <div className="flex justify-between items-center w-full max-w-screen-xl mx-auto">
+        {/* Logo on the Left */}
+        <div className="flex-shrink-0">
+          <img
+            src={isDarkMode ? logoLight : logoDark}
+            alt="Logo"
+            className="h-10 w-auto"
+          />
+        </div>
+
+        {/* Desktop Navigation and Dark Mode Toggle to the Right */}
+        <div className="hidden md:flex items-center space-x-5">
+          <nav className="flex space-x-5">
+            <HashLink
+              smooth
+              to="#about"
+              className="hover:underline dark:text-alltext-dark dark:hover:text-highlight-dark text-alltext-light hover:text-highlight-light"
+            >
+              About
+            </HashLink>
+            <HashLink
+              smooth
+              to="#projects"
+              className="hover:underline dark:text-alltext-dark dark:hover:text-highlight-dark text-alltext-light hover:text-highlight-light"
+            >
+              Projects
+            </HashLink>
+            <HashLink
+              smooth
+              to="#contact"
+              className="hover:underline dark:text-alltext-dark dark:hover:text-highlight-dark text-alltext-light hover:text-highlight-light"
+            >
+              Contact
+            </HashLink>
+            <a
+              href="/mammolitiResume.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:underline dark:text-alltext-dark dark:hover:text-highlight-dark text-alltext-light hover:text-highlight-light"
+            >
+              Resume
+            </a>
+          </nav>
+
+          {/* Dark Mode Toggle Button */}
+          <button
+            onClick={toggleDarkMode}
+            aria-label="Toggle Dark Mode"
+            className="ml-6 text-[20px] p-2 focus:outline-none bg-transparent dark:text-alltext-dark text-alltext-light"
+          >
+            {isDarkMode ? <FiSun /> : <FiMoon />}
+          </button>
+        </div>
+
         {/* Hamburger for mobile */}
-        <div className="md:hidden absolute left-4 top-4 z-50">
+        <div className="md:hidden absolute right-4 top-4 z-50">
           <button
             onClick={toggleActiveClass}
             aria-label="Toggle navigation menu"
@@ -30,88 +85,42 @@ function Header({ toggleDarkMode, isDarkMode }) {
             )}
           </button>
         </div>
-
-        {/* Desktop Navigation */}
-        <nav className="hidden md:flex justify-center space-x-5">
-          <HashLink
-            smooth
-            to="#about"
-            className="hover:underline dark:text-alltext-dark dark:hover:text-highlight-dark text-alltext-light hover:text-highlight-light"
-          >
-            About
-          </HashLink>
-          <HashLink
-            smooth
-            to="#projects"
-            className="hover:underline dark:text-alltext-dark dark:hover:text-highlight-dark text-alltext-light hover:text-highlight-light"
-          >
-            Projects
-          </HashLink>
-          <HashLink
-            smooth
-            to="#contact"
-            className="hover:underline dark:text-alltext-dark dark:hover:text-highlight-dark text-alltext-light hover:text-highlight-light"
-          >
-            Contact
-          </HashLink>
-          <a
-            href="/mammolitiResume.pdf"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="hover:underline dark:text-alltext-dark dark:hover:text-highlight-dark text-alltext-light hover:text-highlight-light"
-          >
-            Resume
-          </a>
-        </nav>
-
-        {/* Dark Mode Toggle Button */}
-        <button
-          onClick={toggleDarkMode}
-          aria-label="Toggle Dark Mode"
-          className="absolute right-4 top-4 z-50 text-[20px] p-2 focus:outline-none bg-transparent dark:text-alltext-dark text-alltext-light"
-        >
-          {isDarkMode ? <FiSun /> : <FiMoon />}
-        </button>
-
-        {/* Mobile Navigation */}
-        <nav
-          className={`absolute top-0 right-0 w-screen h-screen md:hidden bg-[#F7F9F9] dark:bg-[#2C3E50] backdrop-filter p-6 shadow-[rgba(0,_0,_0,_1)_0px_30px_90px] z-40 rounded-b-lg flex justify-center items-center ${
-            isActive ? "block" : "hidden"
-          }`}
-        >
-          <ul className="flex flex-col justify-center items-center gap-y-8">
-            <li className="text-[20px] block px-4 py-2 dark:text-alltext-dark dark:hover:text-highlight-dark text-alltext-light hover:text-highlight-light">
-              <HashLink smooth to="#about" onClick={() => setIsActive(false)}>
-                About
-              </HashLink>
-            </li>
-            <li className="text-[20px] block px-4 py-2 dark:text-alltext-dark dark:hover:text-highlight-dark text-alltext-light hover:text-highlight-light">
-              <HashLink
-                smooth
-                to="#projects"
-                onClick={() => setIsActive(false)}
-              >
-                Projects
-              </HashLink>
-            </li>
-            <li className="text-[20px] block px-4 py-2 dark:text-alltext-dark dark:hover:text-highlight-dark text-alltext-light hover:text-highlight-light">
-              <HashLink smooth to="#contact" onClick={() => setIsActive(false)}>
-                Contact
-              </HashLink>
-            </li>
-            <li className="text-[20px] block px-4 py-2 mb-32 dark:text-alltext-dark dark:hover:text-highlight-dark text-alltext-light hover:text-highlight-light">
-              <a
-                href="/mammolitiResume.pdf"
-                target="_blank"
-                rel="noopener noreferrer"
-                onClick={() => setIsActive(false)}
-              >
-                Resume
-              </a>
-            </li>
-          </ul>
-        </nav>
       </div>
+
+      {/* Mobile Navigation */}
+      <nav
+        className={`absolute top-0 right-0 w-screen h-screen md:hidden bg-[#F7F9F9] dark:bg-[#2C3E50] backdrop-filter p-6 shadow-[rgba(0,_0,_0,_1)_0px_30px_90px] z-40 rounded-b-lg flex justify-center items-center ${
+          isActive ? "block" : "hidden"
+        }`}
+      >
+        <ul className="flex flex-col justify-center items-center gap-y-8">
+          <li className="text-[20px] block px-4 py-2 dark:text-alltext-dark dark:hover:text-highlight-dark text-alltext-light hover:text-highlight-light">
+            <HashLink smooth to="#about" onClick={() => setIsActive(false)}>
+              About
+            </HashLink>
+          </li>
+          <li className="text-[20px] block px-4 py-2 dark:text-alltext-dark dark:hover:text-highlight-dark text-alltext-light hover:text-highlight-light">
+            <HashLink smooth to="#projects" onClick={() => setIsActive(false)}>
+              Projects
+            </HashLink>
+          </li>
+          <li className="text-[20px] block px-4 py-2 dark:text-alltext-dark dark:hover:text-highlight-dark text-alltext-light hover:text-highlight-light">
+            <HashLink smooth to="#contact" onClick={() => setIsActive(false)}>
+              Contact
+            </HashLink>
+          </li>
+          <li className="text-[20px] block px-4 py-2 mb-32 dark:text-alltext-dark dark:hover:text-highlight-dark text-alltext-light hover:text-highlight-light">
+            <a
+              href="/mammolitiResume.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+              onClick={() => setIsActive(false)}
+            >
+              Resume
+            </a>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
