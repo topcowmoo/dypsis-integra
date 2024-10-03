d907b1370610b49107aad0a9a10d9e58cd86ba4a
Salvatore <mammoliti.sam@gmail.com>
2024-10-02 22:02:56 -0400

    removed additional project section for future dev

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -7,7 +7,6 @@ const projectData = [
     image: "https://portfoliobucket2024.s3.amazonaws.com/baketomo.webp",
     githubLink: "https://github.com/topcowmoo/futuristic-wound",
     renderLink: "https://baketomo.onrender.com",
-    isHidden: false,
   },
   {
     id: 2,
@@ -17,7 +16,6 @@ const projectData = [
     image: "https://portfoliobucket2024.s3.amazonaws.com/jate.webp",
     githubLink: "https://github.com/topcowmoo/general-toothpaste",
     renderLink: "https://general-toothpaste.onrender.com",
-    isHidden: false,
   },
   {
     id: 3,
@@ -27,7 +25,6 @@ const projectData = [
     image: "https://portfoliobucket2024.s3.amazonaws.com/cream.webp",
     githubLink: "https://github.com/topcowmoo/unbreakable-cream",
     herokuLink: "https://unbreakable-cream-a4b95d823c2c.herokuapp.com/",
-    isHidden: false,
   },
   {
     id: 4,
@@ -37,7 +34,6 @@ const projectData = [
     image: "https://portfoliobucket2024.s3.amazonaws.com/drymvc.webp",
     githubLink: "https://github.com/topcowmoo/dry-mvc-tech-blog",
     herokuLink: "https://dry-mvc-tech-blog-b103946e224c.herokuapp.com/",
-    isHidden: false,
   },
   {
     id: 5,
@@ -48,7 +44,6 @@ const projectData = [
     githubLink: "https://github.com/topcowmoo/windy-e-commerce-site",
     demoLink:
       "https://drive.google.com/file/d/1q_LhyzKBZ6-6fw9hbPgev_rvDWNz-TUh/view",
-    isHidden: false,
   },
   {
     id: 6,
@@ -58,16 +53,7 @@ const projectData = [
     image: "https://portfoliobucket2024.s3.amazonaws.com/weather.webp",
     githubLink: "https://github.com/topcowmoo/effective-weather-interface",
     netlifyLink: "https://effective-weather-interface.netlify.app",
-    isHidden: false,
-  },
-  {  id: 7,
-    title: "Just Another Text Editor",
-    description:
-      "Offline-supported rich text editing within a Progressive Web Application (PWA).",
-    image: "https://portfoliobucket2024.s3.amazonaws.com/jate.webp",
-    githubLink: "https://github.com/topcowmoo/general-toothpaste",
-    renderLink: "https://general-toothpaste.onrender.com",
-    isHidden: true,
+
   },
 ];
 
